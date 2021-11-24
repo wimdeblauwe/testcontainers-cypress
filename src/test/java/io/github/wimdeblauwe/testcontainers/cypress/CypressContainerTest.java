045422e4001f08901a0ff48409df7fe306f1d75d
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-11-24 08:41:21 +0100

    Upgrade to Cypress 9.1.0
    Closes #39

--- src/test/java/io/github/wimdeblauwe/testcontainers/cypress/CypressContainerTest.java
+++ src/test/java/io/github/wimdeblauwe/testcontainers/cypress/CypressContainerTest.java
@@ -19,7 +19,7 @@ class CypressContainerTest {
     void testDefaultDockerImage() {
         CypressContainer container = new CypressContainer();
         container.configure();
-        assertThat(container.getDockerImageName()).isEqualTo("cypress/included:8.7.0");
+        assertThat(container.getDockerImageName()).isEqualTo("cypress/included:9.1.0");
         assertThat(container.getWorkingDirectory()).isEqualTo("/e2e");
     }
 
